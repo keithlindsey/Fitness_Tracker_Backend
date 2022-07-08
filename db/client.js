e0a063df7a99776f720f314b8e1ede99912ec8df
keithlindsey <99703783+keithlindsey@users.noreply.github.com>
2022-07-07 21:03:47 -0400

    dude come on

--- db/client.js
+++ db/client.js
@@ -4,9 +4,11 @@ const { Client } = require('pg');
 
 
 
-const client = new Client({
-    connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/fitness-dev',
-    // ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
-  });
+// const client = new Client({
+//     connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/fitness-dev',
+//     // ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+//   });
+
+ const client = new Client(process.env.DATABASE_URL || "postgres://localhost:5432/fitness-dev");
 
 module.exports = client
